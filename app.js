a060296d84f248309858b7b54015b04c58c0709a
Kuldik <112172212+Kuldik@users.noreply.github.com>
2023-06-05 18:28:26 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const navLink = document.querySelectorAll('.nav__link[data-goto]');
+    if (menuLinks.length > 0) {
+        navLink.forEach(navLink => {
+            navLink.addEventListener('click', onMenuLinkKlick);
+        });
+
+    function onMenuLinkKlick(e) {
+        const navLink = e.target;
+        if(navLink.dataset.goto && document.querySelector(navLink.dataset.goto)){
+            const gotoBlock = document.querySelector(navLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+            // высчитывапет расстояние с учетом вычета высоты intro__inner для корректной прокрутки
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: 'smooth'
+            });
+            e.preventDefault(); // отключение ссылки, для блокировки нежелательного перехода
+        }
+    }
+}
+
+
